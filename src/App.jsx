7add40d9b7d089017873d74e640c50d2b8231a17
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2023-04-04 15:38:43 +0100

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 
+import React,{useState} from "react";
 import './App.css';
+import Categories from './Categories';
+import Items from "./data";
+import Menu from './Menu';
+
+const allCategories = ["all", ...new Set(Items.map((Item) => Item.category))]
+console.log(allCategories)
 
 function App() {
+const [menuItems, setMenuItems] = useState(Items)
+const [categories, setCategories] = useState(allCategories)
+
+const filterItems = (category) => {
+  if(category === "all") {
+    setMenuItems(Items)
+    return;
+  }
+  const newItems = Items.filter((Item) => Item.category === category)
+  setMenuItems(newItems)
+}
+
   return (
-    <div className="App">
-<h1>testing uot</h1>
+<main>
+  <section className="menu section">
+    <div className="title">
+      <h2>our menu</h2>
+      <div className="underline"></div>
     </div>
+    <Categories categories={categories} filterItems={filterItems}/>
+    <Menu Items={menuItems}/>
+  </section>
+</main>
   );
 }
 
---
+++ src/data.jsx
@@ -0,0 +1,85 @@
+import Image1 from "./assets/24530-ButtermilkPancakesII-mfs-beauty-3385-4x3-69cdcbfc09fa4aa999e2a3e18b8d68a8.jpg"
+import Image2 from "./assets/2832.jpg.webp"
+import Image3 from "./assets/a2fa507ee9aafaadf0aef6e1932ed65223-30-grubstreet-takeout.rhorizontal.w700.jpg.webp"
+import Image4 from "./assets/cheesecake-factory-oreo-dream-extreme-cheesecake-copycat-recipe_1.jpg"
+import Image5 from "./assets/images.jpeg"
+import Image6 from "./assets/img_182461_countrydelightcofounders.jpg"
+import Image7 from "./assets/nathans.jpg.webp"
+import Image8 from "./assets/persimmon-caprese-6-300x300.jpg.webp"
+import Image9 from "./assets/tumblr_piukhbBYdY1wow359o1_1280.jpg"
+
+const menu = [
+ {
+   id: 1,
+   title: 'buttermilk pancakes',
+   category: 'breakfast',
+   price: 15.99,
+   img: Image1,
+   desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+ },
+ {
+   id: 2,
+   title: 'diner double',
+   category: 'lunch',
+   price: 13.99,
+   img: Image2,
+   desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+ },
+ {
+   id: 3,
+   title: 'godzilla milkshake',
+   category: 'shakes',
+   price: 6.99,
+   img: Image3,
+   desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+ },
+ {
+   id: 4,
+   title: 'country delight',
+   category: 'breakfast',
+   price: 20.99,
+   img: Image4,
+   desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+ },
+ {
+   id: 5,
+   title: 'egg attack',
+   category: 'lunch',
+   price: 22.99,
+   img: Image5,
+   desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+ },
+ {
+   id: 6,
+   title: 'oreo dream',
+   category: 'shakes',
+   price: 18.99,
+   img: Image6,
+   desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+ },
+ {
+   id: 7,
+   title: 'bacon overflow',
+   category: 'breakfast',
+   price: 8.99,
+   img: Image7,
+   desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+ },
+ {
+   id: 8,
+   title: 'american classic',
+   category: 'lunch',
+   price: 12.99,
+   img: Image8,
+   desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+ },
+ {
+   id: 9,
+   title: 'quarantine buddy',
+   category: 'shakes',
+   price: 16.99,
+   img: Image9,
+   desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+ },
+];
+export default menu;
